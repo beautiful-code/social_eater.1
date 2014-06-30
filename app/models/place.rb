81cee69612f9609d28c76e7053ef8a1fd3b7c8ef
Ravi Bhim <ravi.bhim@yahoo.com>
2014-06-30 18:24:05 +0530

    showing items by category

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,8 @@ class Place < ActiveRecord::Base
   has_many :items
   has_many :categories, :order => "position ASC"
 
+  def ordered_items
+    items.group_by(&:category_id).sort_by {|cat,items| Category.find(cat).position}
+  end
+
 end
