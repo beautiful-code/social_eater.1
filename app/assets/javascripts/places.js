48c95367dfe14af9c455fdccd7ee50166afaf0cb
Ravi Bhim <ravi.bhim@yahoo.com>
2014-07-11 17:44:26 +0530

    alert box on item save errors

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,25 +1,22 @@
 $(function () {
 
     $('.edit-item').focusout(function (event) {
-
         setTimeout(function () {
             if (!event.delegateTarget.contains(document.activeElement)) {
                $(event.delegateTarget).closest('form').submit();
                $(event.delegateTarget).closest('form').find('.progress-spin').removeClass('hidden');
             }
         }, 0);
-
-
     })
 
 
     $('.edit-item').closest('form').bind('ajax:complete', function() {
-
         $(this).find('.progress-spin').addClass('hidden');
     });
 
 
     $('document').ajaxError(function() {
+        alert('Error saving the item...');
         $( ".updated-item-progress" ).text( "Error submitting details." );
     });
 
