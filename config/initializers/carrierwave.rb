6c2269a2355e3301f7bca9f8be0ce2445b01112e
Pradeep G <itsprdp@gmail.com>
2014-07-20 22:37:33 +0530

    Carrierwave public urls instead

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = ENV['S3_BUCKET_NAME']
-  config.fog_public = false
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 
 end
